f0e91fad7c2dd2d995a382c8d66f1947bd007c5a
mursaleen <muhammad.mursaleen@fiveriverstech.com>
2018-12-19 17:07:09 +0500

    push added

--- app/src/main/java/com/firstbit/fifaworldcup2018highlights/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/firstbit/fifaworldcup2018highlights/fcm/MyFirebaseMessagingService.java
@@ -1,7 +1,17 @@
 package com.firstbit.fifaworldcup2018highlights.fcm;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.firstbit.fifaworldcup2018highlights.MainActivity;
+import com.firstbit.fifaworldcup2018highlights.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -15,5 +25,39 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // message, here is where that should be initiated.
 //        Log.e(TAG, "From: " + remoteMessage.getFrom());
         Log.e(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        displayCustomNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+    }
+    private void displayCustomNotification(String title, String desc) {
+        int num = (int) System.currentTimeMillis();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        int notificationId = 1;
+        String channelId = "channel-01";
+        String channelName = "Channel Name";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(
+                    channelId, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.logo)
+                .setContentTitle(title)
+                .setAutoCancel(true)
+                .setContentText(desc);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Bundle newsFeedBundle = new Bundle();
+        intent.putExtras(newsFeedBundle);
+        stackBuilder.addNextIntent(intent);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, num , intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        notificationManager.notify(num, mBuilder.build());
     }
 }
