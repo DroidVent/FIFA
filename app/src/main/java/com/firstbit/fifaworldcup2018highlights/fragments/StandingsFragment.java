d83f48b2e5209b4ece28aaab4b9d767b98b6c6a2
mursaleen <muhammad.mursaleen@fiveriverstech.com>
2019-01-07 19:10:15 +0500

    get standings

--- app/src/main/java/com/firstbit/fifaworldcup2018highlights/fragments/StandingsFragment.java
+++ app/src/main/java/com/firstbit/fifaworldcup2018highlights/fragments/StandingsFragment.java
@@ -13,22 +13,27 @@ import android.widget.Spinner;
 
 import com.firstbit.fifaworldcup2018highlights.R;
 import com.firstbit.fifaworldcup2018highlights.data.Standing;
+import com.firstbit.fifaworldcup2018highlights.data.Video;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class StandingsFragment extends Fragment {
 
     private View rootView;
-
+    private ArrayList<String> leagues = new ArrayList<>();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.standing_layout, container, false);
-//        init();
+        init();
         initSpinner();
         return rootView;
     }
@@ -40,17 +45,32 @@ public class StandingsFragment extends Fragment {
         list.add("Mercury");
         list.add("Mercury");
         list.add("Mercury");
-// Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_item, list);
-// Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
         spinner.setAdapter(adapter);
     }
 
     private void init() {
-        DatabaseReference mDatabase;
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        Query myRef;
+        myRef = database.getReference("league_standings");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                    Object object = singleSnapshot.getValue();
+                    Log.e("", "");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("", "onCancelled", databaseError.toException());
+            }
+        });
+    }
+     /*   DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference("standings");
         Standing standing = new Standing();
         standing.setD("0");
         standing.setGA("0");
@@ -73,5 +93,5 @@ public class StandingsFragment extends Fragment {
                         Log.e("", "fail");
                     }
                 });
-    }
+    }*/
 }
